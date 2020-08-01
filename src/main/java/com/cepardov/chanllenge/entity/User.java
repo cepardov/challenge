ed8cc2b52eaed90fb9cded19f746b4bcf93e1504
Cristian Pardo <cpardo@kiosolutions.com>
2020-07-31 22:01:06 -0400

    Optimize imports

--- src/main/java/com/cepardov/chanllenge/entity/User.java
+++ src/main/java/com/cepardov/chanllenge/entity/User.java
@@ -1,10 +1,12 @@
 package com.cepardov.chanllenge.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 /**
