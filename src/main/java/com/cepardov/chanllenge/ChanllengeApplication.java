2bcf13278727c3e3f366107e9718265868fc6d82
Cristian Pardo <cpardo@kiosolutions.com>
2020-07-31 21:35:03 -0400

    initial commit

---
+++ src/main/java/com/cepardov/chanllenge/ChanllengeApplication.java
@@ -0,0 +1,13 @@
+package com.cepardov.chanllenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanllengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanllengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cepardov/chanllenge/ChanllengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepardov.chanllenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanllengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
