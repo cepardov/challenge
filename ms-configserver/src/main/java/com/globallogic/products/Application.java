da1573ee7306f4701cfd23ad76c34f0a13b925a2
Cristian Pardo <cepardov@gmail.com>
2025-05-23 22:54:19 -0400

    feat: develop: configure config server

--- ms-configserver/src/main/java/com/globallogic/products/Application.java
+++ ms-configserver/src/main/java/com/globallogic/products/Application.java
@@ -2,7 +2,9 @@ package com.globallogic.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class Application {
 
