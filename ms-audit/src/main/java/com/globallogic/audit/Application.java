985e36a0c48f3c69a23b8194c6a24d67b1e5afe1
Cristian Pardo <cepardov@gmail.com>
2025-06-10 23:18:57 -0400

    feat: develop: add ms-audit

--- ms-audit/src/main/java/com/globallogic/audit/Application.java
+++ ms-audit/src/main/java/com/globallogic/audit/Application.java
@@ -1,4 +1,4 @@
-package com.globallogic.inventory;
+package com.globallogic.audit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
